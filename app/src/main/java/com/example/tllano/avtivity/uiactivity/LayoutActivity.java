6afb03f41a03f418a6e1e977e852793715162ea8
TLLANO <TLLANO@163.com>
2016-07-31 20:53:59 +0800

    continuity Add and remove views of Activity 连续添加和移除视图的 Activity
    Signed-off-by: TLLANO <TLLANO@163.com>

--- app/src/main/java/com/example/tllano/avtivity/uiactivity/LayoutActivity.java
+++ app/src/main/java/com/example/tllano/avtivity/uiactivity/LayoutActivity.java
@@ -10,9 +10,10 @@ import android.widget.LinearLayout;
 import com.example.tllano.avtivity.R;
 import com.example.tllano.avtivity.activity.BaseActivity;
 
-public class LayoutActivity extends BaseActivity {
+public class LayoutActivity extends BaseActivity  implements View.OnClickListener{
 
     LinearLayout mContainer;
+    private int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,8 @@ public class LayoutActivity extends BaseActivity {
         setContentView(R.layout.activity_layout);
 
         mContainer = (LinearLayout) findViewById(R.id.verticalContainer);
+        findViewById(R.id.add_btn).setOnClickListener(this);
+        findViewById(R.id.remove_btn).setOnClickListener(this);
     }
 
     //
@@ -38,4 +41,30 @@ public class LayoutActivity extends BaseActivity {
                 ActionBar.LayoutParams.WRAP_CONTENT));
     }
 
+    private void addButtonView() {
+        i++;
+        Button button = new Button(this);
+        button.setText("button" + i);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        button.setLayoutParams(params);
+        mContainer.addView(button, 0);
+    }
+
+    private void removeButtonView() {
+        if (i > 0) {
+            mContainer.removeViewAt(0);
+        }
+        i--;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.add_btn) {
+            addButtonView();
+        }
+        if (v.getId() == R.id.remove_btn) {
+            removeButtonView();
+        }
+    }
 }
