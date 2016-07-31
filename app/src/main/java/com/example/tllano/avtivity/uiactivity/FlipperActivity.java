c8eef32825a52cc4af16f22b5cb124cab1f491ba
TLLANO <TLLANO@163.com>
2016-07-31 20:17:33 +0800

    XML version of the AnimatorSet version of the flip animation 带有 XML 版本 AnimatorSet 的翻转器动画
    Signed-off-by: TLLANO <TLLANO@163.com>

--- app/src/main/java/com/example/tllano/avtivity/uiactivity/FlipperActivity.java
+++ app/src/main/java/com/example/tllano/avtivity/uiactivity/FlipperActivity.java
@@ -1,6 +1,8 @@
 package com.example.tllano.avtivity.uiactivity;
 
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.graphics.Bitmap;
@@ -14,7 +16,8 @@ import com.example.tllano.avtivity.activity.BaseActivity;
 
 public class FlipperActivity extends BaseActivity {
     private boolean mIsHeads;
-    private ObjectAnimator mFlipper;
+//    private ObjectAnimator mFlipper;
+    private AnimatorSet mFlipper;
     private Bitmap mHeadsImage, mTailsImage;
     private ImageView mFlipImage;
 
@@ -27,13 +30,24 @@ public class FlipperActivity extends BaseActivity {
         mTailsImage = BitmapFactory.decodeResource(getResources(), R.drawable.banana_pic);
 
         mFlipImage = (ImageView) findViewById(R.id.flip_image);
-        mFlipImage.setImageBitmap(mHeadsImage);
+//        mFlipImage.setImageBitmap(mHeadsImage);
+        mFlipImage.setImageResource(R.drawable.apple_pic);
         mIsHeads = true;
 
         // 设置y轴旋转
+        /*
         mFlipper = ObjectAnimator.ofFloat(mFlipImage, "translationY  ", 0f, 360f);
         mFlipper.setDuration(500);
-        mFlipper.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+        */
+
+        // 根据xml加载AnimatorSet
+        mFlipper = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.flip);
+        // 将动画附加到适当的目标试图
+        mFlipper.setTarget(mFlipImage);
+        ObjectAnimator flipAnimator = (ObjectAnimator) mFlipper.getChildAnimations().get(0);
+
+//        mFlipper.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+        flipAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 if(animation.getAnimatedFraction() >= 0.25f && mIsHeads) {
